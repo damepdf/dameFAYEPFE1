31b03299ca27800ba73f58d3c00cf37d31073fbe
damepdf <parraindamefaye@gmail.com>
2017-07-16 19:24:53 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace bootSoutenance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Timer timer1;
+        public System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telegram;
+using System.Threading;
+
+namespace bootSoutenance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Telegram.bot bot = new Telegram.bot();
+            Telegram.JSON json = new Telegram.JSON();
+            bot.token = "385203321:AAHtRnTNPkndAkj9ikgF7d3vBw0HQSbmapU";
+            //bot.token = "446997055:AAGFfl8jrSkF5gB17zbiKlh78wsBfmvxCUw";
+            CheckForIllegalCrossThreadCalls = false;
+            backgroundWorker1.RunWorkerAsync();
+            bot.update = "true";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void robot()
+        {
+            
+            bot.update = "true";
+                if (bot.message_text != "".ToLower()) {
+
+                    if (bot.message_text == "/id")
+                    {
+                        bot.sendMessage.send(bot.chat_id, bot.chat_id.ToString());
+                    }
+                    else if (bot.message_text == "galilee")
+                    {
+                        bot.sendMessage.parse_mode = "html";
+                        bot.sendMessage.send(bot.chat_id, "<a href=\"http://www.sup-galilee.univ-paris13.fr/index.php\">emploi du temps sup galilee</a>");
+                    }
+                    
+                    // else
+                    // {
+                    //bot.sendMessage.send(bot.chat_id, "bot comprend pas ☹️");
+                    // }
+
+                }
+                
+
+            
+        }
+       
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Telegram.bot.update = "true";
+            Telegram.bot.Automatic_answer.textMessage("anglais incontournable", "merci pour la reponse");
+            Telegram.bot.Automatic_answer.textMessage("toiec determine pas le niveau d'anglais", "merci pour la reponse");
+            Thread dame = new Thread(robot);
+            Thread.Sleep(1);
+            dame.IsBackground = true;
+            dame.Start();
+        }
+
+     
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int a = 1;
+            int b = 0;
+            while (true)
+            {
+
+                bot.update = "true";
+                if (bot.message_text == "2")
+                {
+                    bot.send_inline_keyboard.send(bot.chat_id, "question 1");
+                    bot.send_inline_keyboard.keyboard_R1_1 = "pour 👍";
+                    bot.send_inline_keyboard.keyboard_R1_1_callback_data = "up";
+                    bot.send_inline_keyboard.keyboard_R1_2 = "contre 👎";
+                    bot.send_inline_keyboard.keyboard_R1_2_callback_data = "down";
+                    bot.send_inline_keyboard.send(bot.chat_id, "valider le toiec pour avoir le diplome");
+                }
+                if (bot.data == "up")
+                {
+                    // a += 1;
+                    //bot.editMessageInlinekeyboard.send(bot.chat_id, bot.message_id, "question : " + a.ToString());
+                    // bot.editMessageInlinekeyboard.keyboard_R1_1 = "👍";
+                    //bot.editMessageInlinekeyboard.keyboard_R1_1_callback_data = "up";
+                    //bot.editMessageInlinekeyboard.keyboard_R1_2 = "👎";
+                    //bot.editMessageInlinekeyboard.keyboard_R1_2_callback_data = "down";
+                    //bot.editMessageInlinekeyboard.send(bot.chat_id, bot.message_id, "votant pour " + a.ToString());
+                    // bot.send_inline_keyboard.keyboard_R1_1 = "pourquoi ";
+                    bot.send_inline_keyboard.send(bot.chat_id, "question 2");
+                    bot.send_inline_keyboard.send(bot.chat_id, "pourquoi etes vous POUR?");
+                    Telegram.bot.Automatic_answer.textMessage("anglais incontournable", "merci pour la reponse");     
+                   
+                   // bot.editMessageInlinekeyboard.send(bot.chat_id, bot.message_id, "question : " + a.ToString());
+
+                }
+
+                if (bot.data == "down")
+                {
+                    //b += 1;
+                    //bot.editMessageInlinekeyboard.keyboard_R1_1 = "👍";
+                    //bot.editMessageInlinekeyboard.keyboard_R1_1_callback_data = "up";
+                    //bot.editMessageInlinekeyboard.keyboard_R1_2 = "👎";
+                    //bot.editMessageInlinekeyboard.keyboard_R1_2_callback_data = "down";
+                    //bot.editMessageInlinekeyboard.send(bot.chat_id, bot.message_id, "votant contre " + b.ToString());
+                    //bot.send_inline_keyboard.keyboard_R1_1 = "pourquoi ";
+                    bot.send_inline_keyboard.send(bot.chat_id, "question 2");
+                    Telegram.bot.Automatic_answer.textMessage("toiec determine pas le niveau d'anglais", "merci pour la reponse");
+
+                    bot.send_inline_keyboard.send(bot.chat_id, "pourquoi etes vous CONTRE?");
+
+                }
+
+                if (bot.message_text == "/id")
+                {
+                    bot.sendMessage.send(bot.chat_id, bot.chat_id.ToString());
+                }
+
+                if (bot.message_text == "start")
+                {
+                    bot.sendMessage.send(bot.chat_id, " choix 1: infos formation \n choix : 2 questionnaire evaluation\n");
+
+                    // Telegram.bot.Automatic_answer.textMessage("1", "pas disponiblr");
+
+                }
+                if (bot.message_text == "1")
+                {
+                    bot.sendMessage.parse_mode = "html";
+                    bot.sendMessage.send(bot.chat_id, "<a href=\"http://www.sup-galilee.univ-paris13.fr/index.php\">Telecom info et emploi du temps</a>");
+                }
+
+              
+            
+
+            }
+       }
+    }
+}
